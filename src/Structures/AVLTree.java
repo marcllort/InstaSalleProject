39bdd365b0897b842077e0a6196047198ad384fe
mac12llm <mac12llm@gmail.com>
2019-04-10 09:37:30 +0200

    InsertTo fix, not tested

--- src/Structures/AVLTree.java
+++ src/Structures/AVLTree.java
@@ -16,9 +16,9 @@ public class AVLTree<E> {                                                       
     public void addElement(int key, E element, String elementKey) {                                                             // Funció de add nodes simplificada, comprova si tenim root inicial
         if (root == null) {                                                                                                     // En cas de no tenir-ne, el crea, i en cas de tenir-ne crida la inserció normal a partir de root
             root = new Node<>(key, element, elementKey);
-            return;
+        } else {
+            insertTo(root, key, element, elementKey);
         }
-        insertTo(root, key, element, elementKey);
     }
 
 
@@ -26,23 +26,25 @@ public class AVLTree<E> {                                                       
         if (key == node.getKey()) {
             System.out.println("Ja existeix aquest node! No s'ha pogut inserir el node: " + key + "(" + elementName + ")");
 
-        } else if (node.getRightSon() != null && key > node.getKey()) {
-            insertTo(node.getRightSon(), key, element, elementName);                                                            // Crida recursiva, on el root sera el fill dret
-            node.setHeight(calculateHeight(node));                                                                              // Recalculem alçada, del node pare, un cop el node ja ha sigut inserit
-            balance(node, key);                                                                                                 // Un cop inserit el node, es possible que ens calgui balancejar el arbre
+        } else if (key > node.getKey()) {
+            if (node.getRightSon() == null) {                                                                                   // Si no existeix el node dret, creem el node i el posem
+                node.setRightSon(new Node<>(key, element, elementName));                                                        // En cas de caldre un balanceig, es faria als nivells superiors, i si ja som als fills del root no cal fer-ne
+                node.setHeight(recalculateHeight(node));                                                                        // Recalculem la alçada del node
+            } else {
+                insertTo(node.getRightSon(), key, element, elementName);                                                        // Crida recursiva, on el root sera el fill dret
+                node.setHeight(recalculateHeight(node));                                                                        // Recalculem alçada, del node pare, un cop el node ja ha sigut inserit
+                balance(node, key);                                                                                             // Un cop inserit el node, es possible que ens calgui balancejar el arbre
+            }
 
-        } else if (node.getRightSon() == null && key > node.getKey()) {                                                         // Si no existeix el node dret, creem el node i el posem
-            node.setRightSon(new Node<>(key, element, elementName));                                                            // En cas de caldre un balanceig, es faria als nivells superiors, i si ja som als fills del root no cal fer-ne
-            node.setHeight(calculateHeight(node));                                                                              // Recalculem la alçada del node
-
-        } else if (node.getLeftSon() != null && key < node.getKey()) {
-            insertTo(node.getLeftSon(), key, element, elementName);                                                             // Crida recursiva, on el root sera el fill esquerre
-            node.setHeight(calculateHeight(node));                                                                              // Recalculem alçada, del node pare, un cop el node ja ha sigut inserit
-            balance(node, key);                                                                                                 // Un cop inserit el node, es possible que ens calgui balancejar el arbre
-
-        } else if (node.getLeftSon() == null && key < node.getKey()) {                                                          // Si no existeix el node esquerra, creem el node i el posem
-            node.setLeftSon(new Node<>(key, element, elementName));                                                             // En cas de caldre un balanceig, es faria als nivells superiors, i si ja som als fills del root no cal fer-ne
-            node.setHeight(calculateHeight(node));                                                                              // Recalculem la alçada del node
+        } else if (key < node.getKey()) {
+            if (node.getLeftSon() == null) {                                                                                    // Si no existeix el node esquerra, creem el node i el posem
+                node.setLeftSon(new Node<>(key, element, elementName));                                                         // En cas de caldre un balanceig, es faria als nivells superiors, i si ja som als fills del root no cal fer-ne
+                node.setHeight(recalculateHeight(node));                                                                        // Recalculem la alçada del node
+            } else {
+                insertTo(node.getLeftSon(), key, element, elementName);                                                         // Crida recursiva, on el root sera el fill esquerre
+                node.setHeight(recalculateHeight(node));                                                                        // Recalculem alçada, del node pare, un cop el node ja ha sigut inserit
+                balance(node, key);                                                                                             // Un cop inserit el node, es possible que ens calgui balancejar el arbre
+            }
         }
 
     }
@@ -67,13 +69,13 @@ public class AVLTree<E> {                                                       
         node.setElementsName(left.getElementsName());                                                                           // Copiem el seu nom
 
         if (node.getLeftSon() != null) {                                                                                        // En cas de no ser null, al haver hagut una rotació, cal calcular-li la nova alçada
-            node.getLeftSon().setHeight(calculateHeight(node.getLeftSon()));
+            node.getLeftSon().setHeight(recalculateHeight(node.getLeftSon()));
         }
         if (node.getRightSon() != null) {                                                                                        // En cas de no ser null, al haver hagut una rotació, cal calcular-li la nova alçada
-            node.getRightSon().setHeight(calculateHeight(node.getRightSon()));
+            node.getRightSon().setHeight(recalculateHeight(node.getRightSon()));
         }
 
-        node.setHeight(calculateHeight(node));                                                                                  // Recalculem l'alçada del node inicial
+        node.setHeight(recalculateHeight(node));                                                                                  // Recalculem l'alçada del node inicial
 
 
     }
@@ -98,13 +100,13 @@ public class AVLTree<E> {                                                       
         node.setElementsName(right.getElementsName());                                                                           // Copiem el seu nom
 
         if (node.getLeftSon() != null) {                                                                                         // En cas de no ser null, al haver hagut una rotació, cal calcular-li la nova alçada
-            node.getLeftSon().setHeight(calculateHeight(node.getLeftSon()));
+            node.getLeftSon().setHeight(recalculateHeight(node.getLeftSon()));
         }
         if (node.getRightSon() != null) {                                                                                        // En cas de no ser null, al haver hagut una rotació, cal calcular-li la nova alçada
-            node.getRightSon().setHeight(calculateHeight(node.getRightSon()));
+            node.getRightSon().setHeight(recalculateHeight(node.getRightSon()));
         }
 
-        node.setHeight(calculateHeight(node));                                                                                   // Recalculem l'alçada del node inicial
+        node.setHeight(recalculateHeight(node));                                                                                   // Recalculem l'alçada del node inicial
 
 
     }
