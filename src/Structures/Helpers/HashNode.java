3941fdce9108f1c8f445879ac7299545efed45af
prodriguezd <37362780+prodriguezd@users.noreply.github.com>
2019-05-03 16:39:29 +0200

    Add files via upload

---
+++ src/Structures/Helpers/HashNode.java
@@ -0,0 +1,55 @@
+package Structures.Helpers;
+
+import Model.Post;
+import Model.User;
+
+/**
+ * https://www.geeksforgeeks.org/hashtable-in-java/
+ */
+
+public class HashNode<E>{
+
+
+    //atributos
+    private int key;                                                                    //Identifica a los elementos
+    private E element;                                                                  //Elemento a guardar
+    private HashNode<E> nextElement;                                                    //Siguiente nodo que tenga la misma Key que el anterior
+
+    private HashNode(int key, E element) {
+        this.key = key;
+        this.element = element;
+        nextElement = null;
+    }
+
+    private HashNode() {
+        element = null;
+        nextElement = null;
+        key = 0;
+    }
+    //Getters y Setters
+
+    public int getKey() { return key; }
+
+    public void setKey(int key) { this.key = key; }
+
+    public E getElement() { return element; }
+
+    public void setElement(E element) { this.element = element; }
+
+    public HashNode<E> getNextElement() { return nextElement; }
+
+    public void setNextElement(HashNode<E> nextElement) { this.nextElement = nextElement; }
+
+
+
+
+    public String getNomElement() {
+        if(element instanceof User) {
+            return ((User) element).getUsername();
+        }
+        else {
+            return ((Post) element).toString();
+        }
+    }
+}
+
