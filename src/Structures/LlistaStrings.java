23261d323a181f3427267fda00dc1b4ed2b6bed1
Alex Almansa <alex.almansa.2016@salleurl.edu>
2019-05-20 11:41:21 +0200

    Added class to search words

---
+++ src/Structures/LlistaStrings.java
@@ -0,0 +1,28 @@
+package Structures;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class LlistaStrings {
+    private String paraula;
+    private int pes;
+
+    public LlistaStrings(String paraula, int pes){
+        this.paraula = paraula;
+        this.pes = pes;
+    }
+
+
+
+    public String getParaula() {
+        return paraula;
+    }
+
+    public int getPes() {
+        return pes;
+    }
+}
+
+
+
+
